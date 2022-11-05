9cb267ca8f5cf626a51d26b59c898c186e48f62d
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-11-05 15:08:24 +0200

    SearchCountries v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
+let elRef = selector => document.querySelector(selector);
+
+elRef('#search-box').addEventListener('input', debounce(onInputEnter, DEBOUNCE_DELAY));
+
+function onInputEnter(evt) { 
+    const name = (evt.target.value).trim();
+    if (name === "") {
+        elRef('.country-list').innerHTML = "";
+        elRef('.country-info').innerHTML = "";
+    } else { 
+        fetchCountries(name)
+        .then((countries) => console.log(countries))
+        .catch((error) => console.log(error));
+    }
+}
